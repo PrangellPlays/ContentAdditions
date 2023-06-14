eef9a5d8cfbeb3500ab717f563980eacfe2160ad
PrangellPlays <ewanprangell@gmail.com>
2023-06-14 18:13:27 +0100

    new commit

---
+++ src/main/java/net/prangellplays/contentsmpsummoner/block/custom/SummonerOfTheAirshipFunction.java
@@ -0,0 +1,40 @@
+package net.prangellplays.contentsmpsummoner.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.SetBlockCommand;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Position;
+import net.minecraft.world.World;
+
+public class SummonerOfTheAirshipFunction extends Block {
+
+    public SummonerOfTheAirshipFunction(Settings settings) {
+        super(settings);
+    }
+
+
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            world.setBlockState(this.getDefaultState(pos), Blocks.AIR.getDefaultState());
+            Entity customEntity = ((EntityType<Entity>) EntityType.get("minecraft:sheep").get()).create(world);
+            customEntity.setPosition(0, 100, 0);
+            world.spawnEntity(customEntity);
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+    private BlockPos getDefaultState(BlockPos pos) {
+        return pos;
+    }
+}
