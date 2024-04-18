cb87f3f773fb58d90326e7af50330ed056d590bd
PrangellPlays <prangellplays@gmail.com>
2024-04-18 21:52:54 +0100

    Edited summoner of the airship

--- src/main/java/net/prangellplays/contentadditions/block/SummonerOfTheAirshipBlock.java
+++ src/main/java/net/prangellplays/contentadditions/block/SummonerOfTheAirshipBlock.java
@@ -3,14 +3,23 @@ package net.prangellplays.contentadditions.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.YOffset;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SummonerOfTheAirshipBlock extends Block {
 
@@ -25,7 +34,7 @@ public class SummonerOfTheAirshipBlock extends Block {
         if (!world.isClient) {
             world.setBlockState(this.getDefaultState(pos), Blocks.AIR.getDefaultState());
             Entity customEntity = ((EntityType<Entity>) EntityType.get("expedition:airship").get()).create(world);
-            customEntity.setPosition(0, 100, 0);
+            customEntity.setPosition(pos.getX(), 100, pos.getZ());
             world.spawnEntity(customEntity);
         }
 
--- src/main/java/net/prangellplays/contentadditions/registry/ContentItems.java
+++ src/main/java/net/prangellplays/contentadditions/registry/ContentItems.java
@@ -1,7 +1,9 @@
 package net.prangellplays.contentadditions.registry;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import net.prangellplays.contentadditions.ContentAdditions;
 import net.minecraft.item.Item;
 import net.minecraft.util.registry.Registry;
@@ -10,6 +12,13 @@ import java.util.ArrayList;
 
 @SuppressWarnings("unused")
 public class ContentItems {
+
+    public static final Item ICON = registerItem("icon", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(ContentAdditions.MOD_ID, name), item);
+    }
+
     public static final ArrayList<Item> MOD_ITEMS = new ArrayList<>();
 
     public static void init() {}
@@ -19,4 +28,5 @@ public class ContentItems {
         list.add(item);
         return item;
     }
+
 }
